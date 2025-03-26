af25a0d6559d861872dbdda12456dbca2dead030
amitvishwa19 <amitvishwa19@gmail.com>
2025-03-26 19:39:00 +0530

    shad cn

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import '@/css/globals.css';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,7 +19,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`} suppressHydrationWarning={true}>
+      <body
+        className={`${geistSans.variable} 
+        ${geistMono.variable} antialiased`}
+        suppressHydrationWarning={true}
+      >
         {children}
       </body>
     </html>
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,18 @@
+'use client'
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className=" text-3xl">
       Next js App
+      <Button
+        variant={'outkine'}
+        onClick={() => { console.log('btn click') }}
+        className=' hover:cursor-pointer'
+      >
+        Button
+      </Button>
     </div>
   );
 }
