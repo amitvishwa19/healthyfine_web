ad36d627dd252b0da1c2416b1effe2a747b86974
amitvishwa19 <amitvishwa19@gmail.com>
2025-03-26 19:49:32 +0530

    bg color

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className=" text-3xl">
+    <div className=" text-3xl bg-red-400">
       Next js App
       <Button
         variant={'outkine'}
